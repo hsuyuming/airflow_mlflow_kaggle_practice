c6c44bc93f603e9384b2a7762cf3b85c0bbf9a15
hsuyuming <abego452@gmail.com>
2020-02-21 04:20:54 +0800

    fix all of the bug

--- dags/src/pusher.py
+++ dags/src/pusher.py
@@ -11,8 +11,11 @@ def save_model_local(**kwargs):
     logging.info('Save model')
 
     ti = kwargs['ti']
-    # xgboost, lightgbm = ti.xcom_pull(task_ids='model_validation')
-    xgboost = ti.xcom_pull(task_ids='model_validation')
+    xgboost, lightgbm = ti.xcom_pull(task_ids='model_validation')
+    # xgboost = ti.xcom_pull(task_ids='model_validation')
 
     path = os.path.join(kwargs['path_model_save_dir'], 'xgboost')
     mlflow.xgboost.save_model(xgboost, path)
+
+    path = os.path.join(kwargs['path_model_save_dir'], 'lightgbm')
+    mlflow.lightgbm.save_model(lightgbm, path)
